2a28c500eff77bbc936f4114a777f8872c08f139
penhsokra <penhsokra@gmail.com>
2022-07-11 18:21:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Test test test
         </a>
       </header>
     </div>
